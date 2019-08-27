0db323be098bd035ddd57c4272f1ca80b9022e9f
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-08-27 23:03:46 +0530

    update

--- play.py
+++ play.py
@@ -11,14 +11,14 @@ cam.set(4,height)
 # give the camera to warm up
 time.sleep(1)
 count = 0
-background = 0
+back_ground = 0
 
 for i in range(60):
-    return_val, background = cam.read()
+    return_val, back_ground = cam.read()
     if return_val == False:
         continue
 
-background = np.flip(background, axis=1)  # flipping of the frame
+back_ground = np.flip(back_ground, axis=1)  # flipping of the frame
 
 while (cam.isOpened()):
     return_val, img = cam.read()
@@ -27,18 +27,18 @@ while (cam.isOpened()):
     count = count + 1
     img = np.flip(img, axis=1)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([100,150,0])
-    upper_red = np.array([140,255,255])
-    mask1 = cv2.inRange(hsv, lower_red, upper_red)
-    lower_red = np.array([100,150,0])
-    upper_red = np.array([140,255,255])
-    mask2 = cv2.inRange(hsv, lower_red, upper_red)
+    lower_blue = np.array([100,150,0])
+    upper_blue = np.array([140,255,255])
+    mask1 = cv2.inRange(hsv, lower_blue, upper_blue)
+    lower_blue = np.array([100,150,0])
+    upper_blue = np.array([140,255,255])
+    mask2 = cv2.inRange(hsv, lower_blue, upper_blue)
     mask1 = mask1 + mask2
     mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3),
                                                             np.uint8), iterations=2)
     mask1 = cv2.dilate(mask1, np.ones((3, 3), np.uint8), iterations=1)
     mask2 = cv2.bitwise_not(mask1)
-    res1 = cv2.bitwise_and(background, background, mask=mask1)
+    res1 = cv2.bitwise_and(back_ground, back_ground, mask=mask1)
     res2 = cv2.bitwise_and(img, img, mask=mask2)
     final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
     cv2.imshow("magic frame", final_output)
